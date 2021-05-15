14876ca61a0e320f62f8eabaeb6621ab0f839cd6
Sweety Ahmed <sweety127ahmed@gmail.com>
2021-05-15 20:08:59 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import faker from "faker";
+	import Post from "./Post.svelte";
+
+	let posts = [];
+
+	function gen() {
+		// loop and generate
+		for (let i = 0; i < 50; i++) {
+			// Generate
+			const username = faker.name.findName();
+			const image = `https://robohash.org/${encodeURI(
+				username
+			)}.png?set=set4`;
+			const likes = Math.round(Math.random() * 1000);
+			const comments = Math.round(Math.random() * 1000);
+
+			// Add to array
+			posts.push({ username, image, likes, comments });
+		}
+		// copy old array as new array to old variable (for updating svelte)
+		posts = [...posts];
+	}
+
+	gen();
+</script>
+
+<div class="posts">
+	{#each posts as p}
+		<Post data={p} />
+	{/each}
+</div>
+
+<style>
+	.posts {
+		max-width: 30rem;
+		margin: auto;
+	}
+</style>
---
+++ src/Post.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let data;
+</script>
+
+<div class="post">
+    <div class="header">
+        <p>{data.username}</p>
+    </div>
+    <div class="image">
+        <img src={data.image} alt="" />
+    </div>
+    <div class="footer">
+        <p>{data.likes} Likes</p>
+        <p>{data.comments} Comments</p>
+    </div>
+</div>
+
+<style>
+    .post {
+        border: 1px solid #aaa;
+        border-radius: 3px;
+        margin-bottom: 2rem;
+        background: #fff;
+    }
+
+    .header {
+        padding: 0.5rem 2rem;
+        border-bottom: 1px solid #aaa;
+        font-weight: bold;
+    }
+
+    .image {
+        border-bottom: 1px solid #aaa;
+    }
+
+    .footer {
+        padding: 0.5rem 2rem;
+    }
+</style>
